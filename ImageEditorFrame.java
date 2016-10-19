39a2b1b4989798f6199774f9c3512f3e29f76377
Behrendt <Amelie.Behrendt@haw-hamburg.de>
2016-10-19 15:19:11 +0200

    added ImageEditorPanel.java, new method (setDummyImage) and new panel in ImageEditorFrame.java

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -6,10 +6,26 @@ import java.io.*;
 import javax.imageio.*;
 
 public class ImageEditorFrame extends JFrame{
+	
+	ImageEditorPanel panel;
+	
 	public ImageEditorFrame(){
+		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(400, 300);
 		setVisible(true);
 		setTitle("Image Editor");
+		
+		panel = new ImageEditorPanel();
+		add(panel);
+		setDummyImage();
 	}
+	
+	private void setDummyImage(){
+		BufferedImage bufferedImage = new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB);
+		Graphics g = bufferedImage.getGraphics();
+		g.setColor(Color.YELLOW);
+		g.fillOval(10, 10, 380, 280);
+		panel.setImage(bufferedImage);
+		}
 }
